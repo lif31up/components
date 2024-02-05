97f0e9d1a289a3ea0b703e504ad3e8697f827a89
ﾚﾉｷ乇1ᑌᑭ <nulranlcns9991@gmail.com>
2024-02-05 13:35:37 +0900

    feature: 버튼을 통해 팝업을 끄고 닫을 수 있습니다.

--- component/feature/PopUp.tsx
+++ component/feature/PopUp.tsx
@@ -16,16 +16,21 @@ function PopUp({
 }: PopUp): ReactPortal | null {
   const style: TailwindProperties = {
     sm: "",
-    base: "",
+    base: "w-screen h-screen grid justify-items-center items-center fixed top-0 left-0 z-50 bg-neutral-500",
   };
   return isOpen
     ? ReactDOM.createPortal(
-        <button
-          onClick={onClose}
-          className={`${style.sm} ${style.base} ${className}`}
-        >
-          {children}
-        </button>,
+        <section className={`${style.sm} ${style.base} ${className}`}>
+          <div className="w-fit h-fit p-4 bg-white">
+            {children}
+            <button
+              onClick={onClose}
+              className="w-fit h-fit p-4 text-base font-bold bg-neutral-200 shadow mt-8"
+            >
+              닫기
+            </button>
+          </div>
+        </section>,
         document.body
       )
     : null;
--- component/section/PopUpSection.tsx
+++ component/section/PopUpSection.tsx
@@ -2,12 +2,13 @@
 
 import React, { useState } from "react";
 import PopUp from "@/component/feature/PopUp";
-import Heading from "@/component/common/Heading";
 import Default from "@/util/interface";
 import TailwindProperties from "@/util/tailwindProperties";
+import Heading from "@/component/common/Heading";
 
 function PopUpSection({ className }: Default) {
-  const [isPopUp, setIsPupUp] = useState(true);
+  const [isPopUp, setIsPupUp] = useState(false);
+  const openPopUp = () => setIsPupUp(true);
   const closePopUp = () => setIsPupUp(false);
   const style: TailwindProperties = {
     sm: "sm:w-full sm:px-80 sm:py-6",
@@ -15,18 +16,29 @@ function PopUpSection({ className }: Default) {
   };
   return (
     <section className={`${style.sm} ${style.base} ${className}`}>
-      <PopUp
-        isOpen={isPopUp}
-        onClose={closePopUp}
-        className="w-full h-16 bg-neutral-500 py-2 text-base text-white font-bold fixed top-0 left-0 z-50"
-      >
-        <h1>PopUp.tsx</h1>
+      <Heading
+        data={{
+          filename: "LoginWindow.tsx",
+          title: "로그인 창",
+          desc: "사용자가 로그인 정보를 입력할 수 있게 하는 요소입니다. 리액트 후크 폼을 통해 작성했습니다.",
+          href: "https://github.com/lif31up/components",
+        }}
+      />
+      <div className="w-full grid justify-items-center items-center">
+        <button
+          onClick={openPopUp}
+          className="w-full h-fit px-4 py-4 text-base bg-neutral-300 shadow"
+        >
+          팝업 뛰우기
+        </button>
+      </div>
+      <PopUp isOpen={isPopUp} onClose={closePopUp}>
+        <h1 className="text-xl font-bold">PopUp.tsx</h1>
         <p>
           리액트돔 포탈을 이용해서 팝업을 구현했습니다. 눌러서 제거할 수
           있습니다.
         </p>
       </PopUp>
-      <div className="h-16 w-full" />
     </section>
   );
 }
