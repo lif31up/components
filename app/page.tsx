96012d33111885a2485593a3eaf039eab461deb0
ﾚﾉｷ乇1ᑌᑭ <nulranlcns9991@gmail.com>
2024-02-05 13:02:03 +0900

    feature: PopUpSection이 새롭게 추가됩니다.
    눌러서 제거하는 팝업 형식을 만들어 추가합니다.

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ScrollerSection from "@/component/section/ScrollerSection";
 import LoginSection from "@/component/section/LoginSection";
+import PopUpSection from "@/component/section/PopUpSection";
 
 export default function Home(): React.JSX.Element {
   return (
     <main>
+      <PopUpSection />
       <ScrollerSection className="mt-4" />
       <LoginSection className="mt-4" />
     </main>
