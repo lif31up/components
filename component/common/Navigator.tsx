11b8f9635b6197e85d99e1ca9f24516bbc5e34ea
ﾚﾉｷ乇1ᑌᑭ <nulranlcns9991@gmail.com>
2024-02-06 00:06:59 +0900

    Update Navigator.tsx

--- component/common/Navigator.tsx
+++ component/common/Navigator.tsx
@@ -1,23 +1,37 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 import Default from "@/utils/interface";
 import TailwindProperties from "@/utils/tailwindProperties";
 import { SetterOrUpdater, useRecoilState } from "recoil";
 import { authToken } from "@/libs/auth";
 
-function Navigator({ className, id }: Default) {
+function Navigator({ className }: Default) {
   const [token, setToken]: [null | string, SetterOrUpdater<any>] =
     useRecoilState(authToken);
+  const id: string = "top-bar";
+  useEffect((): void => {
+    const target: HTMLElement | null = document.getElementById(id);
+    if (!target) return;
+    target.style.transition = "0.5s ease-in-out";
+    window.addEventListener("scroll", (): void => {
+      if (window.scrollY > 1) {
+        target.style.height = "3.75rem";
+      } else {
+        target.style.height = "4rem";
+      }
+    });
+  }, []);
   const style: TailwindProperties = {
-    base: "absolute",
+    md: "",
+    base: "fixed top-0 left-0 bg-neutral-100 w-full h-16 flex items-center justify-center shadow",
   };
   return (
-    <section id={id} className={`${style.base} ${className}`}>
-      <div className="w-full h-16 px-8 fixed top-0 left-0 z-40 bg-neutral-200">
+    <>
+      <div className={`${style.sm} ${style.base} ${className}`} id={id}>
         <h1 className="text-xl font-bold">{token}</h1>
       </div>
-    </section>
+    </>
   );
 }
 export default Navigator;
