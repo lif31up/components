68701c72c8368126dcaaeb9bd38b96c51ea5bb0c
ﾚﾉｷ乇1ᑌᑭ <nulranlcns9991@gmail.com>
2024-02-06 10:34:37 +0900

    fix: 내용을 수정했습니다.

--- component/section/TokenSection.tsx
+++ component/section/TokenSection.tsx
@@ -20,7 +20,7 @@ function TokenSection({ className }: Default) {
           data={{
             filename: "TokenMaker.tsx",
             title: "토큰 발행기",
-            desc: "토큰을 발행하고 안전하게 저장합니다. 그리고 저장한 토큰에 따라서 리액트 요소들이 리렌더링됩니다.",
+            desc: "토큰을 발행하고 이를 클라이언트 쿠키와 리코일 아톰에 저장합니다. 따라서 만약 인증에 대한 변화가 생길 때마다 이를 구독하는 요소들을 리렌더링됩니다.",
             href: "https://github.com/lif31up/components",
           }}
         />
