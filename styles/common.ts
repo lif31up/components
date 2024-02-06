74771370af6caaa13bdd3a80b72646143af88fe6
ﾚﾉｷ乇1ᑌᑭ <nulranlcns9991@gmail.com>
2024-02-06 10:32:25 +0900

    heading에 대한 스타일 정의에 관한 상수를 추가합니다.

---
+++ styles/common.ts
@@ -0,0 +1,9 @@
+import TailwindProperties from "@/utils/tailwindProperties";
+
+export const heading: TailwindProperties = {
+  xl: "xl:px-80",
+  lg: "lg:px-60",
+  md: "md:px-40",
+  sm: "sm:px-8",
+  base: "w-full px-6",
+};
